cadf1dd388b234600bfcddf9015baf93555b7779
Marie Ihle Frogner <marieihlefrogner@gmail.com>
2020-02-18 16:08:53 +0100

    edit script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-echo "Starting CTF container..."
+echo "Starting CTF Tools..."
 docker run --rm -v $PWD:/pwd --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -d --name ctf -i ctf:ubuntu19.10 > /dev/null
 docker exec -it ctf /bin/zsh
 
-echo "Stopping CTF container..."
+echo "Stopping CTF Tools..."
 docker stop ctf > /dev/null
